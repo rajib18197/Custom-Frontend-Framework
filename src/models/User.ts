18b0e39b1d87a17aeecc56ec6fa93137d09a4be4
rajib18197 <rajib18197@gmail.com>
2024-08-15 13:54:33 +0600

    implemented specific model before refactor to a reusable resource

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,9 +1,14 @@
 interface UserProps {
+  id?: number;
   name?: string;
   age?: number;
 }
 
+type Callback = () => void;
+
 export class User {
+  events: { [key: string]: Callback[] } = {};
+
   constructor(private data: UserProps) {}
 
   get(key: keyof UserProps): string | number | undefined {
@@ -12,5 +17,50 @@ export class User {
 
   set(update: UserProps): void {
     Object.assign(this.data, update);
+  }
+
+  on(eventName: string, callback: Callback): void {
+    this.events[eventName] = this.events[eventName]
+      ? [...this.events[eventName], callback]
+      : [callback];
+  }
+
+  trigger(eventName: string): void {
+    const handlers: Callback[] | undefined = this.events[eventName];
+
+    if (!handlers || !handlers.length) return;
+
+    handlers.forEach((callback: Callback) => {
+      callback();
+    });
+  }
+
+  fetch(): void {
+    fetch(`http://localhost:3000/users/${this.get("id")}`)
+      .then((res: Response) => {
+        return res.json();
+      })
+      .then((data: UserProps) => {
+        this.set(data);
+      })
+      .catch((err: Error) => {
+        console.error(err);
+      });
+  }
+
+  save(): void {
+    const id = this.get("id");
+
+    if (id) {
+      fetch(`http://localhost:3000/users/${id}`, {
+        method: "PATCH",
+        body: JSON.stringify(this.data),
+      });
+    } else {
+      fetch(`http://localhost:3000/users/`, {
+        method: "POST",
+        body: JSON.stringify(this.data),
+      });
+    }
   }
 }
